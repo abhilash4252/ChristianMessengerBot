f41a7cb9c85b6e8993e60692aee5db5b390100ea
AK <rabhilash@student.nitw.ac.in>
2018-01-03 17:56:35 +0530

    typo in verify_token

--- controllers/verification.js
+++ controllers/verification.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   const hubChallenge = req.query['hub.challenge'];
   const hubMode = req.query['hub.mode'];
-  const verifyTokenMatches = (req.query['hub.verify_token'] === 'christiangameBot');
+  const verifyTokenMatches = (req.query['hub.verify_token'] === 'christiangamebot');
   if (hubMode && verifyTokenMatches) {
     res.status(200).send(hubChallenge);
   } else {
