da26d577a27cc118ea9f8ffb61f1d9d490cceefc
AK <rabhilash@student.nitw.ac.in>
2018-01-03 17:49:17 +0530

    made port dynamic for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Webhook server is listening, port 3000'));
 
 const verificationController = require('./controllers/verification');
 const messageWebhookController = require('./controllers/messageWebhook');
